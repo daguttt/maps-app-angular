deb0d8b12a8c23bb4d3530582dae400703f09815
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-29 09:31:29 -0500

    Class 308: Application Menu

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  path: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+  menuItems: MenuItem[] = [
+    { path: '/maps/fullscreen', title: 'Fullscreen' },
+    { path: '/maps/bookmarks', title: 'Marcadores' },
+    { path: '/maps/zoom-range', title: 'Zoom range' },
+    { path: '/maps/properties', title: 'Propiedades' },
+  ];
+  constructor() {}
 }
